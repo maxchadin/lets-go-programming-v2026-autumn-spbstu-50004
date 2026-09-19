f7b48f123b30482f84d73fc528d91d5d19f33881
Maxim Chadim <maxchadin080418@gmail.com>
2026-09-19 03:11:19 +0300

    [TASK-1] corrected the error in the assignment err1 and err2

--- chadin.maxim/task-1/cmd/service/main.go
+++ chadin.maxim/task-1/cmd/service/main.go
@@ -8,13 +8,13 @@ func main() {
 	var op1, op2 int
 	var operation string
 
-	_, err1 = fmt.Scan(&op1)
+	_, err1 := fmt.Scan(&op1)
 	if err1 != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	_, err2 = fmt.Scan(&op2)
+	_, err2 := fmt.Scan(&op2)
 	if err2 != nil {
 		fmt.Println("Invalid second operand")
 		return
