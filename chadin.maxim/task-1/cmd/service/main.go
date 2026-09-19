ecb35fb83108a17ecf17acd3f5fccfc73a44259e
Maxim Chadim <maxchadin080418@gmail.com>
2026-09-19 03:05:34 +0300

    [TASK-1] added handling of division by zero

--- chadin.maxim/task-1/cmd/service/main.go
+++ chadin.maxim/task-1/cmd/service/main.go
@@ -30,6 +30,10 @@ func main() {
 	case "*":
 		fmt.Println(op1 * op2)
 	case "/":
+		if op2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
 		fmt.Println(op1 / op2)
 	default:
 		fmt.Println("Invalid operation")
